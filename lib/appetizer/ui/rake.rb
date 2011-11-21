b65f39008466f6a9b58550ee4a3d9db87b4c492e
John Barnette <john@jbarnette.com>
2011-11-21 09:41:33 -0600

    Be more picky about what assets get compiled.

--- lib/appetizer/ui/rake.rb
+++ lib/appetizer/ui/rake.rb
@@ -14,8 +14,10 @@ task :compile => :init do
 
   manifest = {}
 
-  App.assets.each_logical_path do |path|
+  App.assets.each_file do |path|
     next if File.basename(path).start_with? "_"
+    next if %r|app/views| =~ path.to_s and not %r|app/views/client| =~ path.to_s
+    next if path.to_s.end_with? ".jst.jade" # HACK
 
     if asset = App.assets[path]
       manifest[path] = asset.digest_path
